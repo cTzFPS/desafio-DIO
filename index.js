499831de3106879b07f85b86c27d9fca1ad1744b
cTzFPS <gm777skr@gmail.com>
2023-12-05 18:17:41 -0300

    projeto completo

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-let nome = "Gabriel"
-console.log(nome)
+let Heroi = "DioHero"
+let xp = 1000
+
+if (xp <= 1000 <= 2000){
+    console.log(Heroi + " Ferro")
+}else if (xp => 2001 <= 3000 ){
+    console.log(Heroi + " Prata")
+}else if (xp => 3001 <= 4000){
+    console.log(Heroi + " ouro")
+}
